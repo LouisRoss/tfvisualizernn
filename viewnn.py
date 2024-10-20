3bda3036c4b9c660a50f00c72e56d09235d9df9c
Louis Ross <louis.ross@gmail.com>
2024-10-20 13:27:47 -0700

    View full population mode, population 0 only

--- viewnn.py
+++ viewnn.py
@@ -21,7 +21,17 @@ fileparse = r'^([a-zA-Z]+)(\d*)$'
 df_iris = px.data.iris()
 fig = px.scatter(df_iris, x='sepal_width', y='sepal_length', color='species', size='petal_length', hover_data=['petal_width'])
 """
+
+# NOTE: Using global variables will mean individual sessions will interfere with each other.
+#       However, it is unreasonable to load and analyze a simulation on every update, so.
+xedgepos = []
+yedgepos = []
+rows = []
+currentrow = 0
+
+
 def GeneratePositions(size: int):
+    global xedgepos, yedgepos
     edgesize = int(math.sqrt(size))
     xedgesize = edgesize
     yedgesize = edgesize
@@ -35,7 +45,21 @@ def GeneratePositions(size: int):
             xedgepos.append(x)
             yedgepos.append(y)
 
-    return (xedgepos, yedgepos)
+
+def LoadSimulation(simnumber):
+    global rows, currentrow, xedgepos, yedgepos
+    with open('/record/simulation' + str(simnumber) + '/spikes/spike0.csv') as csvfile:
+        csvreader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        rows = []
+        for row in csvreader:
+            rows.append(row)
+
+    if len(rows) == 0:
+        rows.append(np.random.randn(784))
+    GeneratePositions(len(rows[0]))
+
+    currentrow = 0
+
 
 def GetSimulationNumbers():
   sims = []
@@ -50,47 +74,18 @@ def GetSimulationNumbers():
   return sims
 
 
-rows = []
-currentrow = 0
-def LoadSimulation(simnumber):
-    global rows, currentrow
-    with open('/record/simulation' + str(simnumber) + '/spikes/spike0.csv') as csvfile:
-        csvreader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-        rows = []
-        for row in csvreader:
-            rows.append(row)
 
-    if len(rows) == 0:
-        rows.append(np.random.randn(784))
-
-    currentrow = 0
-
-LoadSimulation(0)
-xedgepos, yedgepos = GeneratePositions(len(rows[0]))
-fig = go.Figure(data=go.Scattergl(
-    x = xedgepos,
-    y = yedgepos,
-    dy=2,
-    mode='markers',
-    marker=dict(
-        #color= np.random.randn(784),
-        color = rows[currentrow],
-        colorscale='Viridis',
-        line_width=1,
-        size=20
-    )
-))
-
-app = Dash(__name__, update_title=None)
+app = Dash(__name__, update_title='Spiking Neurons')
 
 
 app.layout = [
     html.Div(className='row', children='Tensorflow Spiking Neural Networks', style={'textAlign':'center', 'fontSize':30}),
     html.Div(className='row', 
         children=[
-            html.Div(children=dcc.Dropdown(GetSimulationNumbers(), GetSimulationNumbers()[0], style={'width':120, 'fontSize':18}, id='sim-dropdown')),
+            html.Div(dcc.Dropdown(GetSimulationNumbers(), GetSimulationNumbers()[1], style={'width':120, 'fontSize':18, 'flex': 1}, id='sim-dropdown')),
+            html.Div(dcc.RadioItems(['Full Population', 'Across Populations'], 'Full Population', inline=True), style={'padding': 10, 'flex': 1}, id='orientation'),
             html.Div()
-        ]),
+        ], style={'display': 'flex', 'flexDirection': 'row'}),
     html.Hr(),
     html.Div(
         children=[
@@ -104,15 +99,25 @@ app.layout = [
     )
 ]
 
+
 @callback(Input('sim-dropdown', 'value'))
 def load_new_simulation(value):
+    global xedgepos, yedgepos
+
+    if value is None:
+        value = 0
+
     LoadSimulation(value)
+
 
 
 @callback(Output('live-update-graph', 'figure'), 
           Input('interval-component', 'n_intervals'))
 def update_graph_live(n):
     global rows, currentrow, xedgepos, yedgepos
+    if len(rows) == 0:
+        LoadSimulation(0)
+
     fig = go.Figure(data=go.Scattergl(
         x = xedgepos,
         y = yedgepos,
